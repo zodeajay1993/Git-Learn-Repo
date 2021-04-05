d491b2e07622395984980804da244411bc8b2ad6
kunal zade <krunalzade9@gmail.com>
2021-04-05 22:39:03 +0530

    CommandLine added

---
+++ CommandLine.java
@@ -0,0 +1,17 @@
+ class CommandLine {
+	
+	public static void main(String  args[]) {
+		
+		int a,b,c;
+		//Scanner sc=new Scanner(System.in);
+		a=Integer.parseInt(args[0]);
+		System.out.println("number one is : "+a);
+		b=Integer.parseInt(args[1]);
+		System.out.println("number two is : "+b);
+		c=a+b;
+		System.out.println("Addition of two numbers is : "+c);
+		
+	}
+
+}
+
